de9901e585c5189a303723f15a23888adc23c265
ManciuAna <manciu_anamaria@yahoo.ro>
2025-03-22 08:36:10 +0000

    components

---
+++ src/features/filter/filterSlice.jsx
@@ -0,0 +1,17 @@
+// filterSlice.js
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = '';
+
+const filterSlice = createSlice({
+  name: 'filter',
+  initialState,
+  reducers: {
+    setFilter(state, action) {
+      return action.payload;
+    },
+  },
+});
+
+export const { setFilter } = filterSlice.actions;
+export default filterSlice.reducer;
